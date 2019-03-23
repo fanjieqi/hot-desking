6df09b487b884fd02b9aa7ebf9395c4d84200d57
fanjieqi <fanjieqi@nibirutech.com>
2019-03-24 01:19:00 +0800

    change the update_profile of user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,4 +14,13 @@ class ApplicationController < ActionController::Base
 
     add_breadcrumb I18n.t("path.home", default: "Home"), :root_path
   end
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
 end
