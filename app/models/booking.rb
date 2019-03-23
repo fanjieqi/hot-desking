2b7b242d8d9834c959d606f466719ca0fd32a60b
fanjieqi <fanjieqi@nibirutech.com>
2019-03-23 17:10:47 +0800

    add user.name

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,2 +1,6 @@
 class Booking < ApplicationRecord
+  belongs_to :user
+  belongs_to :seat
+  delegate :name, to: :user, prefix: :user, allow_nil: true
+  delegate :name, to: :seat, prefix: :seat, allow_nil: true
 end
